607ba67253cf7b747cee2e3fdcd8d9ac5eeca97f
akerbabber <akerbabber@localhost.localdomain>
2015-02-22 23:34:56 +0100

    secondo commit

--- controller/database-controller.php
+++ controller/database-controller.php
@@ -1,10 +1,10 @@
 <?php     //connessione al nostro database
-$connessione_al_server=mysql_connect("amm14_murruMaurizio","murruMaurizio","daino964");  // ip locale, login e password
+$connessione_al_server=mysql_connect("localhost","murruMaurizio","daino964");  // ip locale, login e password
 if(!$connessione_al_server){
     die ('Non riesco a connettermi: errore '.mysql_error()); // questo apparirà solo se ci sarà un errore
 }
 
-$db_selected=mysql_select_db("bloglandia",$connessione_al_server); // dove io ho scritto "prova" andrà inserito il nome del db
+$db_selected=mysql_select_db("amm14_murruMaurizio",$connessione_al_server); // dove io ho scritto "prova" andrà inserito il nome del db
 if(!$db_selected){
     die ('Errore nella selezione del database: errore '.mysql_error()); // se la connessione non andrà a buon fine apparirà questo messaggio
 }
